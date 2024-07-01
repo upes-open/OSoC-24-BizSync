cf0b8b1151a129a67d20d130ff807ed298d793cd
Priyanshu Butola <priyanshubutola@gmail.com>
2024-07-01 19:49:54 +0530

    Added auth pages

---
+++ bizsync-application/src/_auth/pages/Otp.jsx
@@ -0,0 +1,88 @@
+import React, { useState, useRef, useEffect } from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+
+// Define the schema using Zod
+const schema = z.object({
+  email: z.string().email({ message: "Invalid email address" }),
+});
+
+const Otp = () => {
+  const [isOtpValid, setIsOtpValid] = useState(true);
+  const [otp, setOtp] = useState(new Array(4).fill(""));
+  const inputRef = useRef([]);
+  const {
+    resetField,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(schema),
+  });
+
+  useEffect(() => {
+    if (inputRef.current[0]) {
+      inputRef.current[0].focus();
+    }
+  }, []);
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
+  const handleOnChange = (idx, e) => {
+    const value = e.target.value;
+    if (isNaN(value)) return;
+    const newOtp = [...otp];
+
+    newOtp[idx] = value.substring(value.length - 1);
+    setOtp(newOtp);
+
+    if (value && idx < 3 && inputRef.current[idx + 1]) {
+      inputRef.current[idx + 1].focus();
+    }
+  };
+  const handleKeyDown = (idx, e) => {
+    if (
+      e.key === "Backspace" &&
+      !otp[idx] &&
+      idx > 0 &&
+      inputRef.current[idx - 1]
+    ) {
+      inputRef.current[idx - 1].focus();
+    }
+  };
+
+  return (
+    <div className="w-screen min-h-screen bg-[#09090b] text-white flex items-center justify-center fixed">
+      <div className="border-[#27272a] border rounded-md p-8 flex flex-col lg:w-[400px] space-y-3">
+        <p className="font-bold text-2xl">Enter OTP</p>
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="flex flex-col w-full gap-2"
+        >
+          <div className="flex justify-between">
+            {otp.map((val, idx) => (
+              <input
+                type="text"
+                {...resetField("email")}
+                ref={(input) => (inputRef.current[idx] = input)}
+                onChange={(e) => handleOnChange(idx, e)}
+                onKeyDown={(e) => handleKeyDown(idx, e)}
+                value={val}
+                className="w-16 bg-transparent border border-[#27272a] rounded-md py-4 px-3 outline-none focus:border-white transition-all text-lg font-semibold text-center"
+              />
+            ))}
+          </div>
+          {!isOtpValid && <p className="text-red-500 text-sm">Invalid OTP</p>}
+
+          <button className="w-full mt-3 bg-white hover:bg-purple-400 hover:text-white text-black py-2 px-4 rounded transition-all">
+            Continue
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Otp;
---
+++ bizsync-application/src/_auth/pages/ResetPwd.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import { Link } from "react-router-dom";
+
+// Define the schema using Zod
+const schema = z.object({
+  email: z.string().email({ message: "Invalid email address" }),
+});
+
+const ResetPwd = () => {
+  const {
+    resetField,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(schema),
+  });
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <div className="w-screen min-h-screen bg-[#09090b] text-white flex items-center justify-center fixed">
+      <div className="border-[#27272a] border rounded-md p-8 flex flex-col lg:w-1/3 space-y-3">
+        <p className="font-bold text-2xl">Reset Password</p>
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="flex flex-col w-full gap-2"
+        >
+          <div>
+            <p className="text-lg">Email</p>
+            <input
+              type="email"
+              {...resetField("email")}
+              className="w-full bg-transparent border border-[#27272a] rounded-md py-3 px-2 outline-none focus:border-white transition-all"
+              placeholder="abc@example.com"
+            />
+            {errors.email && (
+              <p className="text-red-500 text-sm">{errors.email.message}</p>
+            )}
+          </div>
+
+          <button className="w-full mt-3 bg-white hover:bg-purple-400 hover:text-white text-black py-2 px-4 rounded transition-all">
+            Send Email
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default ResetPwd;
