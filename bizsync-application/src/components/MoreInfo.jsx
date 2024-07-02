8548dcb7230380148c1d505b388779022a6fb0ef
Priyanshu Butola <priyanshubutola@gmail.com>
2024-07-02 22:00:40 +0530

    Dashboard UI

--- bizsync-application/src/components/MoreInfo.jsx
+++ bizsync-application/src/components/MoreInfo.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const MoreInfo = () => {
   return (
-    <div className="bg-[#0B0121] text-white py-16 px-4 flex items-center justify-center font-[Montserrat] min-h-[70vh]">
+    <div className="bg-[#0B0121] text-white py-16 px-4 flex items-center justify-center font-[Montserrat] min-h-[50vh]">
       <div className="bg-gray-800 rounded-lg p-6 flex flex-col shadow-lg shadow-purple-500/20 md:w-1/2 gap-4 items-center">
         <p className="text-3xl font-semibold text-center">
           Our powerful analytics <br /> provides invaluable insights
