95f33f168a533d4ba0a40b8b24382c3d1f4b2bc7
Priyanshu Butola <priyanshubutola@gmail.com>
2024-07-17 05:53:39 +0530

    cors

--- server/dashboard_service/src/middlewares/auth.middleware.js
+++ server/dashboard_service/src/middlewares/auth.middleware.js
@@ -9,7 +9,8 @@ export const verifyJWT = asyncHandler(async (req, res, next) => {
       req.cookies?.accessToken ||
       req.header("Authorization")?.replace("Bearer ", "");
 
-    if (!token) throw new ApiError("Unauthorized request");
+    if (!token) throw new ApiError(401, "Unauthorized request");
+
     const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
 
     const user = await User.findById(decodedToken._id).select(
